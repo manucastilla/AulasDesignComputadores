49d0def25582ec9a5eb1b42cca8e113a909744cb
manucastilla <manuela.castilla@gmail.com>
2020-12-06 21:47:58 -0300

    fazendo o conversor

--- MIPS_A_project/MIPS_A_project.vhd
+++ MIPS_A_project/MIPS_A_project.vhd
@@ -10,11 +10,15 @@ entity MIPS_A_project is
 
   port   (
      CLOCK_50            : in  std_logic;
+	  SW                  : in std_logic_vector(7 DOWNTO 0);
+	  KEY						 : in std_logic_vector(3 DOWNTO 0);
+	  
+	  
 	  PC_out_out : out std_logic_vector(31 DOWNTO 0);
 	  barramentoEndout : out std_logic_vector(31 DOWNTO 0);
 	  seletor_out_out : out std_logic_vector(2 DOWNTO 0);
 	  inA_ULA :  out std_logic_vector(31 DOWNTO 0);
-	inB_ULA :  out std_logic_vector(31 DOWNTO 0);
+	inB_ULA :  out std_logic_vector(31 DOWNTO 0);	
 	mux_lui : out std_logic
 	 
 
@@ -33,6 +37,7 @@ architecture arch_name of MIPS_A_project is
 	signal rd                  : STD_LOGIC;
 	signal saidaBanco_REG2_out : std_logic_vector(31 DOWNTO 0);
 	signal seletor_out : std_logic_vector(2 DOWNTO 0);
+	SIGNAL clk : std_logic;
 
   
 begin
@@ -48,14 +53,12 @@ begin
 					PC_out => PC_out_out,
 					seletor_out => seletor_out,
 					inA_ULA => inA_ULA,
-	inB_ULA => inB_ULA,
-	mux_lui_out => mux_lui
+					inB_ULA => inB_ULA,
+					mux_lui_out => mux_lui
 					
 				);
 
 		
-
-
 	RAM_mips : entity work.RAMMIPS
 				port map (
 					clk      => CLOCK_50,
@@ -66,6 +69,26 @@ begin
 					rd       => rd
 							);
 							
+	
+	edge: entity work.edgeDetector
+				port map(
+					clk    => CLOCK_50,
+              entrada => (not KEY(0)),
+              saida   => clk);
+				);
+				
+	display0 : entity work.conversorHex7Seg
+				port map(
+					 dadoHex => ,
+					 apaga   => , 
+                negativo => , 
+                overFlow => ,
+                -- Output ports
+                saida7seg => 
+				
+				);
+				
+	
 							
 	  barramentoEndout <= barramentoEndereco;
 	  seletor_out_out <= seletor_out;
